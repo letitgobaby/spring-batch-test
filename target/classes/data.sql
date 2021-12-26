ec210ee382fa3c270815fb9c26c29261947cb237
letitgobaby <48303144+letitgobaby@users.noreply.github.com>
2021-12-26 17:11:44 +0900

    remove target folder

--- target/classes/data.sql
+++
@@ -1,7 +0,0 @@
-
-INSERT INTO USER (ID, NAME) 
-VALUES (1, 'kim'), (2, 'lee'), (3, 'park'), (4, 'choi'), (5, 'jeong'), (6, 'jang'), (7, 'go'), (8, 'sin'), (9, 'seo'), (10, 'yu');
-
-
--- INSERT INTO MESSAGE (ID, CONTENT, USER_ID)
--- VALUES (1, 'hello', 1);
